98a6a574a9b52c4a72412a56c6c06ea1c70eaef0
Jaswinder <jassmultani2@gmail.com>
2023-08-25 16:16:13 +0530

    Update meta description index.tsx
    Changed meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ const HomePage: NextPageWithLayout = () => {
     <>
       <MetaTags
         title={`Harmonic | Own your Music`}
-        description={`"NEAR isn’t just a Layer 1 blockchain — it’s the Blockchain Operating System for an  Open Web. Create and discover decentralized apps, and help build the future of the web, today."`}
+        description={`"Create simple profiles to promote your art, events and merchandise."`}
       />
       <NearOrgHomePage />
     </>
